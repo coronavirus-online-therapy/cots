a559e9ecd4bae01b930e6bbd5910426a701b0ca3
Casey Lee <cplee@nektos.com>
2020-03-26 00:04:16 -0700

    login for providers

--- src/App.js
+++ src/App.js
@@ -1,62 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Amplify from 'aws-amplify';
+import Amplify  from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react';
+import { withAuthenticator} from 'aws-amplify-react'; 
 
 Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world
     </div>
   );
 }
-
-const authConfig = {
-  usernameAttributes: 'email',
-  signUpConfig: {
-    header: 'Request a Session',
-    hideAllDefaults: true,
-    signUpFields: [
-      {
-        label: 'Full Name',
-        key: 'name',
-        required: true,
-        displayOrder: 1,
-        type: 'string'
-      },
-      {
-        label: 'Email',
-        key: 'email',
-        required: true,
-        displayOrder: 2,
-        type: 'string'
-      },
-      {
-        label: 'Password',
-        key: 'password',
-        required: true,
-        displayOrder: 3,
-        type: 'password'
-      }
-    ]
-  }
+const signUpConfig =  {
+  hiddenDefaults: ['username', 'phone_number']
 };
-export default withAuthenticator(App, authConfig);
+export default withAuthenticator(App, { usernameAttributes: 'email', signUpConfig, includeGreetings: true });
