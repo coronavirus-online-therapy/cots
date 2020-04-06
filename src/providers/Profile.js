fd17baab80789d03cb0d10bebb25b076c359122a
Tedd Johnson <Github@TeddJohnson.org>
2020-04-05 17:55:48 -0700

    another update on the attestion after hearing fron a lawyer

--- src/providers/Profile.js
+++ src/providers/Profile.js
@@ -291,7 +291,7 @@ function Profile(props) {
                 <Grid item xs={8} align="left">
                   {mode === 'CREATE' && 
                     <FormControlLabel control={
-                      <Checkbox color="primary" required={true}/>} label="I attest that I am a licensed, insured clinician, authorized for unsupervised, solo practice by my state licensing board(s)."/>
+                      <Checkbox color="primary" required={true}/>} label="I attest that I am licensed, insured, and authorized for private practice by my state board(s)."/>
                   }
                 </Grid>
                 <Grid item xs={8} align="left">
