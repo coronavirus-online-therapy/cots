2a46ae34ae9f00880be7c381a620989af76f60af
Austin Abbott <magicaustin51547@gmail.com>
2020-06-03 16:28:19 -0600

    Fix header props object in Therapist Profile

--- src/common/AccessPoints.js
+++ src/common/AccessPoints.js
@@ -95,7 +95,7 @@ function AccessPoints(props) {
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <MaterialTable
                 disabled={props.disabled}
-                header={actions: 'Edit'}
+                header={{ actions: 'Edit' }}
                 columns={[
                     { title: "State", field: "licState", lookup: stateLookup, editable: 'onAdd' },
                     { title: "License", field: "licNum" },
